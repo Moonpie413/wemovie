5204a3b0e655c1c28b5c63a8f00b81bfc6d6418f
wangxh <stormaroon@gmail.com>
2017-05-13 10:59:00 +0800

    1. 去除sun相关的XML解析包

--- src/main/java/personal/wxh/wemovie/config/RootConfig.java
+++ src/main/java/personal/wxh/wemovie/config/RootConfig.java
@@ -1,25 +1,23 @@
 package personal.wxh.wemovie.config;
 
-import com.sun.xml.internal.bind.marshaller.CharacterEscapeHandler;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
 import personal.wxh.wemovie.dataconvater.mappers.Message;
 import personal.wxh.wemovie.props.PropertyGetter;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.IOException;
-import java.io.Writer;
 
 /**
  * Created by wangxh on 17-1-7.
@@ -60,14 +58,7 @@ public class RootConfig {
     public Marshaller marshaller() throws JAXBException {
         Marshaller marshaller = jaxbContext().createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        marshaller.setProperty(CharacterEscapeHandler.class.getName(),
-                new CharacterEscapeHandler() {
-                    @Override
-                    public void escape(char[] ac, int i, int j, boolean flag,
-                                       Writer writer) throws IOException {
-                        writer.write(ac, i, j);
-                    }
-                });
+        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
         return marshaller;
     }
 
--- src/main/java/personal/wxh/wemovie/dataconvater/XMLConverter.java
+++ src/main/java/personal/wxh/wemovie/dataconvater/XMLConverter.java
@@ -1,14 +1,17 @@
 package personal.wxh.wemovie.dataconvater;
 
+import com.sun.xml.bind.marshaller.DataWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-import java.io.*;
 
 /**
  * Created by maroon on 17-1-10.
@@ -17,39 +20,43 @@ import java.io.*;
 @Component
 public class XMLConverter {
 
-    private static final Logger logger = LoggerFactory.getLogger(XMLConverter.class);
+  private static final Logger logger = LoggerFactory.getLogger(XMLConverter.class);
 
-    private Marshaller marshaller;
-    private Unmarshaller unmarshaller;
+  private Marshaller marshaller;
+  private Unmarshaller unmarshaller;
 
-    @Autowired
-    public XMLConverter(Marshaller marshaller, Unmarshaller unmarshaller) {
-        this.marshaller = marshaller;
-        this.unmarshaller = unmarshaller;
+  @Autowired
+  public XMLConverter(Marshaller marshaller, Unmarshaller unmarshaller) {
+    this.marshaller = marshaller;
+    this.unmarshaller = unmarshaller;
+  }
+
+  /**
+   * XML输入流转为对象
+   *
+   * @return 转换后的对象
+   */
+  public Object xml2Object(InputStream inputStream) throws JAXBException {
+    return unmarshaller.unmarshal(inputStream);
+  }
+
+  /**
+   * 对象转换为XML
+   */
+  public String object2XML(Object o) throws IOException, JAXBException {
+    String result;
+    try (StringWriter stringWriter = new StringWriter();) {
+      PrintWriter printWriter = new PrintWriter(stringWriter);
+      DataWriter dataWriter = new DataWriter(printWriter, "UTF-8",
+          (buf, start, len, isAttValue, out) -> {
+            for (int i = start; i < start + len; i++) {
+              char ch = buf[i];
+              out.write(ch);
+            }
+          });
+      marshaller.marshal(o, dataWriter);
+      result = stringWriter.toString();
     }
-
-    /**
-     * XML输入流转为对象
-     * @param inputStream
-     * @return 转换后的对象
-     * @throws JAXBException
-     */
-    public Object xml2Object(InputStream inputStream) throws JAXBException {
-        return unmarshaller.unmarshal(inputStream);
-    }
-
-    /**
-     * 对象转换为XML
-     * @param o
-     * @throws IOException
-     * @throws JAXBException
-     */
-    public String object2XML(Object o) throws IOException, JAXBException {
-        String result;
-        try (StringWriter stringWriter = new StringWriter()){
-            marshaller.marshal(o, stringWriter);
-            result = stringWriter.toString();
-        }
-        return result;
-    }
+    return result;
+  }
 }
